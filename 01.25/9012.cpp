18a6806593e9154dcd1bd75ca5dc8d7ee9aafaaf
Baek-jongwon <63216260+Baek-jongwon@users.noreply.github.com>
2021-01-30 14:27:30 +0900

    Add files via upload

---
+++ 01.25/9012.cpp
@@ -0,0 +1,56 @@
+//# include <iostream>
+//# include <queue>
+//# include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	string str;
+//	queue<char> q;
+//
+//	cin >> n;
+//	
+//	while (n--)
+//	{
+//		int front = 0;
+//		int back = 0;
+//		int cnt = 0;
+//		cin >> str;
+//		for (int i = 0; i < str.length(); i++)
+//		{
+//			q.push(str[i]);
+//		}
+//		while (!q.empty())
+//		{
+//			if (q.front() == '(')
+//			{
+//				front++;
+//			}
+//			else if (q.front() == ')')
+//			{
+//				back++;
+//			}
+//			q.pop();
+//			if (front < back)
+//			{
+//				cnt++;
+//			}
+//		}
+//		if (front != back)
+//		{
+//			cout << "NO\n";
+//		}
+//		else
+//		{
+//			if (cnt > 0)
+//			{
+//				cout << "NO\n";
+//			}
+//			else
+//			{
+//				cout << "YES\n";
+//			}
+//		}
+//	}
+//}
